0f243b8a411542578efeaf3ff958305f0f8ec45e
Andrea Carratta <46504271+devandreacarratta@users.noreply.github.com>
2020-09-19 17:02:23 +0200

    Header - API

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+import { HeaderDTO } from '../dto/header-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getHeader(): Observable<HeaderDTO>{
+    let result = this.httpClient.get<HeaderDTO>(environment.apiUrlHeaderGet);
+    return result;
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,8 @@ export const environment = {
   production: false,
   //baseUrl : `${window.location.protocol}//${window.location.hostname}/portfolio/`,
   //baseUrl : `http://localhost:4200/`,
+  
+  apiUrlHeaderGet: '',
 
   snotifyConfig : {
     showProgressBar: true,
