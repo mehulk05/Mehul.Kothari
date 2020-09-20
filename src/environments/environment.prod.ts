92733a047d6c2cb0bceecd2b158a0151ae873903
Andrea Carratta <46504271+devandreacarratta@users.noreply.github.com>
2020-09-20 15:33:45 +0200

    production json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrlHeaderGet: '/assets/json/header.json',
+  apiUrlIntroGet: '/assets/json/intro.json',
+  apiUrlAboutGet: '/assets/json/about.json',
+  apiUrlExperienceGet: '/assets/json/experience.json',
+  apiUrlSkillGet: '/assets/json/skills.json',
+  apiUrlFooterGet: '/assets/json/footer.json',
+  apiUrlEducationGet: '/assets/json/education.json',
+  apiUrlProjectGet: '/assets/json/projects.json',
 };
